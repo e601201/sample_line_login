045d5a2ea5a8e94a886b20fda60cccb875d1e7a7
e601201 <k.arthur1111@gmail.com>
2022-09-14 23:13:24 +0900

    [feat]line_login_api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#before_login'
   get '/after_login', to: 'static_pages#after_login'
+
+  get 'line_login_api/login', to: 'line_login_api#login'
+  get 'line_login_api/callback', to: 'line_login_api#callback'
 end
